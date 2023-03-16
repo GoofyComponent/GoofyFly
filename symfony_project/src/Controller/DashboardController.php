2e9175cd8aac202a88e9c7784a20655b8e8f31a4
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2023-03-16 15:38:14 +0100

    Commit

--- symfony_project/src/Controller/DashboardController.php
+++ symfony_project/src/Controller/DashboardController.php
@@ -16,8 +16,27 @@ class DashboardController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
+        // get the current user
+        $user = $this->getUser();
+
+        // get the current user's linux credentials
+        $linux_credentials = $user->getLinuxCredentials();
+
+        //Get the username and password
+        $username = $linux_credentials->getUsername();
+        $password = $linux_credentials->getPassword();
+
+        //Get the current web server domain name if it exists, if not use the IP address
+        $domain_name = $_SERVER['SERVER_NAME'];
+        if ($domain_name == "") {
+            $domain_name = $_SERVER['SERVER_ADDR'];
+        }
+
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'username' => $username,
+            'password' => $password,
+            'domain_name' => $domain_name,
         ]);
     }
 }
